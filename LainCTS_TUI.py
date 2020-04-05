96576cdb8a23199fb3a3a8f9fb42b560ee662c95
Tony <SjasFaceMD@gmail.com>
2020-04-05 22:08:50 +0300

    Version one!
    Added TUI via npyscreen

---
+++ LainCTS_TUI.py
@@ -0,0 +1,209 @@
+import npyscreen
+import LainCTS_v1
+
+dictionary = {0:'%CE%BB',
+              1:'%CE%94',
+              2:'sec',
+              3:'inter',
+              4:'lit',
+              5:'music',
+              6:'vis',
+              7:'hum',
+              8:'drug',
+              9:'zzz',
+              10:'layer',
+              11:'q',
+              12:'r',
+              13:'culture',
+              14:'psy',
+              15:'mega',
+              16:'random'}
+CTS = LainCTS_v1.lainChan
+class App(npyscreen.NPSAppManaged):
+    board = None
+    def onStart(self):
+        
+        npyscreen.setTheme(npyscreen.Themes.ColorfulTheme)
+        self.addForm("MAIN", MainForm, name="LainCTS")
+        self.addForm("OP", OPForm, name="Operations")
+        
+class SelectOneBox(npyscreen.BoxTitle):
+    # SelectOne теперь будет окружен боксом
+    _contained_widget = npyscreen.SelectOne
+
+class SelectMultyBox(npyscreen.BoxTitle):
+    # MultiSelect теперь будет окружен боксом
+    _contained_widget = npyscreen.MultiSelect
+
+class OutputBox(npyscreen.BoxTitle):
+    # MultiLineEdit теперь будет окружен боксом
+    _contained_widget = npyscreen.MultiLineEdit 
+
+
+class MainForm(npyscreen.ActionForm):
+    # Конструктор
+    def create(self):
+
+    # Добавляем виджет TitleText/TitleSelectOne на форму
+        self.title = self.add(npyscreen.TitleText, name="Press ok to begin")
+        self.boards = self.add(npyscreen.TitleSelectOne, scroll_exit=True, name='Choose board', values = CTS.get_boards(CTS.BOARDurl)[0])
+    # переопределенный метод, срабатывающий при нажатии на кнопку «cancel»
+    def on_cancel(self):
+        ok_cancel = npyscreen.notify_yes_no("Are you sure you want to exit?", "Warning", editw=2)
+        if ok_cancel:
+            if ok_cancel:
+                self.parentApp.setNextForm(None)
+        else:
+            self.parentApp.setNextFormPrevious
+        
+    # переопределенный метод, срабатывающий при нажатии на кнопку «ok»
+    def on_ok(self):
+        try:
+            Operations = self.parentApp.getForm("OP")
+            Operations.board = dictionary[self.boards.value[0]]
+            self.parentApp.switchForm("OP")
+        except IndexError:
+            Operations = self.parentApp.getForm("OP")
+            Operations.board = dictionary[0]
+            self.parentApp.switchForm("OP")
+        
+class OPForm(npyscreen.ActionForm): 
+    
+    def create(self):
+        self.y, self.x = self.useable_space()
+        self.board = None
+        self.selected = False
+        self.TBRead = False
+        self.threads = []
+        self.texts = []
+        self.resources = []
+        self.output = ''
+        self.OPBox = self.add(SelectOneBox,
+                         scroll_exit=True,
+                         name="Operations",
+                         custom_highlighting=True,
+                         values=["scan cataloge"], 
+                         rely=self.y // 15,
+                         max_width=self.x // 2 - 5,
+                         max_height=self.y // 5)
+        
+        self.OUTBox = self.add(OutputBox,
+                          scroll_exit=True,
+                          name="Output",
+                          relx=self.x // 2,
+                          rely=2)
+    def on_ok(self):
+        try:
+            
+            # scan cataloge function
+            if self.OPBox.value[0] == 0:
+                self.threads = CTS.get_threads(CTS.BOARDurl,
+                                               self.board)
+                for i in self.threads:
+                    self.output += (i+"\n")
+                self.OUTBox.value = self.output + "Cataloge Scanned"
+                self.OPBox.values = ["scan cataloge", "scan thread"]
+                if not self.selected:
+                    self.threadselect = self.add(SelectOneBox,
+                         scroll_exit=True,
+                         name="Threads",
+                         custom_highlighting=True,
+                         values=self.threads, 
+                         rely=self.y // 4,
+                         max_width=self.x // 2 - 5,
+                         max_height=self.y // 4)
+                    self.selected = True
+                else:pass
+            
+            # scan thread function
+            elif self.OPBox.value[0] == 1:
+                try:
+                    threadnum = self.threadselect.value[0] 
+                    self.output = ("OP Post" + "\n" + 
+                                   CTS.get_op_post(self.threads,
+                                                   threadnum) + "\n\n") 
+                    self.texts = CTS.get_texts_and_resources(self.threads,
+                                                             threadnum)[0]
+                    self.output = ''
+                    for i in self.texts:
+                        self.output += (i+"\n")
+                    self.OPBox.values = ["scan cataloge", "scan thread","collect resources","save thread", "read scanned thread"]
+                    self.OUTBox.value = self.output + "Thread Scanned"
+                except IndexError:
+                    self.OUTBox.value = "No Thread Selected"
+            
+            #resource collector function
+            elif self.OPBox.value[0] == 2:
+                try:
+                    threadnum = self.threadselect.value[0] 
+                    self.resources = CTS.get_texts_and_resources(self.threads, threadnum)[1]
+                    CTS.getresources(CTS.BOARDurl, self.resources)
+                    self.output = ''
+                    for i in self.resources:
+                        self.output += (i+"\n")
+                    self.OUTBox.value = self.output + "Resources Collected"
+                except IndexError:
+                    self.OUTBox.value = "No Thread Selected"
+                    
+            elif self.OPBox.value[0] == 3:
+                try:
+                    threadnum = self.threadselect.value[0] 
+                    threadname = CTS.between(self.threads[threadnum], '/res/', '.html')
+                    self.resources = CTS.get_texts_and_resources(self.threads, threadnum)[0]
+                    file = open(CTS.text_folder+"\{}.txt".format(threadname),"w", encoding="utf-8")
+                    op_post = CTS.get_op_post(self.threads, threadnum)
+                    file.write(op_post)
+                    file.write('\n')
+                    for post in self.resources:
+                        file.write(post)
+                        file.write('\n')
+                    file.close()
+                    self.OUTBox.value ="Thread Saved"                    
+                except IndexError:
+                    self.OUTBox.value = "No Thread Selected"
+     
+            elif self.OPBox.value[0] == 4:
+                try:
+                    threadnum = self.threadselect.value[0] 
+                    if not self.TBRead:
+                        self.reader = self.add(SelectMultyBox,
+                                                     scroll_exit=True,
+                                                     name="Read Control",
+                                                     custom_highlighting=True,
+                                                     values=self.texts, 
+                                                     rely=self.y // 2,
+                                                     max_width=self.x // 2 - 5,
+                                                     max_height=self.y // 4)
+                        CTS.op_talk(self.texts[0])
+                        self.reader.values=self.texts[1:]
+                        self.TBRead = True
+                    else:
+                        try:
+                            postnum = self.reader.value
+                            print(postnum)
+                            for i in range(len(postnum)):
+                                print(postnum)
+                                CTS.talk(self.texts[1:], i)
+                        except IndexError:
+                            self.OUTBox.value = "No more posts"
+                except IndexError:
+                    self.OUTBox.value = "No Post Slice selected"
+                    
+        except IndexError:
+            self.OUTBox.value = "Select operation"
+        
+    def on_cancel(self):
+        ok_cancel = npyscreen.notify_yes_no("Are you sure you want to return? \nThis will delete catalog and threads", "Warning", editw=2)
+        if ok_cancel:
+            if ok_cancel:
+                self.OPBox.values = ["scan cataloge"]
+                self.threads = []
+                self.texts = []
+                self.output = ''
+                self.parentApp.setNextForm("MAIN")
+        else:
+            self.parentApp.setNextFormPrevious
+
+
+if __name__ == "__main__":
+    CTS = App().run()
---
+++ LainCTS_v1.py
@@ -0,0 +1,260 @@
+# -*- coding: utf-8 -*-
+
+#start of import list
+import urllib
+import urllib.request
+import urllib.parse
+import requests
+#import threading
+from bs4 import BeautifulSoup
+import pyttsx3
+import os
+#end of import list
+
+
+
+
+
+#------------------------------------------------------------------------------
+#Working code below
+#------------------------------------------------------------------------------
+class lainChan():
+    
+    def __init__(self):
+        self.BOARDurl = 'https://www.lainchan.org'
+        #getting program and resouce paths
+        self.scriptDIR = os.path.dirname(__file__)
+        self.pic_folder = os.path.join(self.scriptDIR, 'PICS')
+        self.text_folder = os.path.join(self.scriptDIR, 'TEXTS')
+        self.pdf_folder = os.path.join(self.scriptDIR, 'PDFS')
+        self.swf_folder = os.path.join(self.scriptDIR, 'SWF')
+        self.webm_folder = os.path.join(self.scriptDIR, 'WEBM')
+        self.gif_folder = os.path.join(self.scriptDIR, 'GIFS')
+        self.tesfolder = os.path.join(self.scriptDIR, 'test')
+
+    def after(self,value, a):
+        # Find and validate first part.
+        pos_a = value.rfind(a)
+        if pos_a == -1: return ""
+        # Returns chars after the found string.
+        adjusted_pos_a = pos_a + len(a)
+        if adjusted_pos_a >= len(value): return ""
+        return value[adjusted_pos_a:]
+
+    def between(self,value, a, b):
+        # Find and validate before-part.
+        pos_a = value.find(a)
+        if pos_a == -1: return ""
+        # Find and validate after part.
+        pos_b = value.rfind(b)
+        if pos_b == -1: return ""
+        # Return middle part.
+        adjusted_pos_a = pos_a + len(a)
+        if adjusted_pos_a >= pos_b: return ""
+        return value[adjusted_pos_a:pos_b]
+
+    def get_boards(self, BOARDurl):
+        '''
+        This will retrieve the boards from the header
+        '''
+        unformated = []
+        formated = []
+        
+        result = requests.get(BOARDurl)
+        catalog = result.content
+        soup = BeautifulSoup(catalog, 'html5lib')
+        links = soup.find_all('a')
+        for link in links:
+            a = link.get('href')
+            if 'index' in a:
+                unformated.append(a)
+            for item in unformated:
+                item = urllib.parse.quote(item)
+                item = self.between(item, '/', '/index')
+                formated.append(item)
+        return unformated, formated    
+      
+
+    def get_threads(BOARDurl, boardNAME):
+    
+        """
+        This extracts the thread catalogue,
+        also extracts headers so you can find catalogues of other boards
+        """
+    
+        #url goes here
+        url = urllib.request.urlopen(BOARDurl +'/'+boardNAME+'/catalog.html')
+        #whole html
+        catalog = url.read()
+        #formated html into BS object
+        soup = BeautifulSoup(catalog, 'html5lib')
+        threads = []    
+        links = soup.find_all('a')
+        for link in links:
+            a = link.get('href')
+            if '/res/' in a:
+                threads.append(a)
+        return threads
+
+
+
+    def get_op_post(threads, threadnum):
+
+       """
+       This will extract the op post
+       """    
+    
+       link = urllib.parse.quote(threads[threadnum])
+       try:
+           url2 = urllib.request.urlopen('https://www.lainchan.org/{}'.format(link))
+           thread = url2.read()
+           soup = BeautifulSoup(thread, 'html5lib')
+           thread = soup.findAll("div", {"class": "post op"})
+           for item in thread:
+               op = item.find('div',{'class':'body'})
+           return op.get_text()
+       except UnicodeEncodeError:
+            print("'ascii' codec can't encode character") 
+            pass
+       except urllib.error.HTTPError:
+            print('Thread is missing, pass')
+            pass
+
+
+
+    def get_texts_and_resources(threads, threadnum):
+    
+        """
+        This will extract thread posts and resourses (images, pdfs etc.)
+        """  
+    
+        texts = []
+        resourses = []
+        #NUMthreads = len(threads)
+        link = urllib.parse.quote(threads[threadnum])
+        try:
+            url2 = urllib.request.urlopen('https://www.lainchan.org/{}'.format(link))
+            thread = url2.read()
+            soup = BeautifulSoup(thread, 'html5lib')
+            postcontainers = soup.findAll("div", {"class": "postcontainer"})
+            NUMposts = len(postcontainers)
+            for posts in postcontainers:
+                body = posts.findAll('div',{'class':'body'})
+                for bodies in body:
+                    text = bodies.get_text()
+                    texts.append(text)
+                    links = soup.find_all('a')
+                    for link in links:
+                        a = link.get('href')
+                        if a != None:
+                            if a not in resourses:
+                                if '/src/' in a:
+                                    if 'http://imgops' not in a and 'http://exif.regex.info/' not in a and 'http://iqdb.org/?url=' not in a:
+                                        if '.jpg' in a or '.png' in a or '.pdf' in a:
+                                            resourses.append(a)
+                                    else: pass
+                                else: pass
+                            else: pass
+                        else:pass
+            NUMposts -= 1
+        except urllib.error.HTTPError:pass
+    #NUMthreads -=1
+    #print('Threads remaining: ', NUMthreads)
+        return (texts, resourses)
+  
+
+    def getresources(self, BOARDurl, THresLinks):
+    
+        '''
+        This will download resources into appropriate folders
+        '''
+    
+        remaining = len(THresLinks)
+        for res in THresLinks:
+            try:
+                if '.jpg' in res or '.png' in res or '.gif' in res:
+                    if 'http://imgops' not in res and 'http://exif.regex.info/' not in res and 'http://iqdb.org/?url=' not in res:
+                        name = self.after(res, 'src/')
+                        res = urllib.parse.quote(res)
+                        retrieveURL = BOARDurl+res
+                        urllib.request.urlretrieve(retrieveURL, lainChan.pic_folder+'\{}'.format(name))
+                if '.pdf' in res:
+                    if 'http://imgops' not in res and 'http://exif.regex.info/' not in res and 'http://iqdb.org/?url=' not in res:
+                        name = res[6:]
+                        res = urllib.parse.quote(res)
+                        retrieveURL = BOARDurl+res
+                        urllib.request.urlretrieve(retrieveURL, lainChan.pdf_folder+'{}'.format(name))
+                else:
+                    pass
+                remaining -= 1
+            except PermissionError:pass
+        
+
+    def op_talk(texts):
+    
+        """
+        This will voice the thread
+        """ 
+    
+        talker = pyttsx3.init()
+        voices = talker.getProperty('voices')
+        talker.setProperty('voice', voices[0].id)
+        talker.say(texts)
+        talker.runAndWait()
+        talker.stop()
+        
+        
+    def talk(texts, idx):
+
+        """
+        This will voice the thread
+        """ 
+        try:
+            talker = pyttsx3.init()
+            voices = talker.getProperty('voices')
+            talker.setProperty('voice', voices[0].id)
+            talker.say(texts[idx])
+            talker.runAndWait()
+            talker.stop()
+        except TypeError:
+            if isinstance(texts, str):
+                talker = pyttsx3.init()
+                voices = talker.getProperty('voices')
+                talker.setProperty('voice', voices[0].id)
+                talker.say(texts)
+                talker.runAndWait()
+                talker.stop()
+            else:pass
+                
+                
+def spiderchan():
+    '''
+    retrieves board urls on keyword, filters sit-on domains
+    you will need a dictionary txt
+    '''
+    links = []
+    ending = ['.org', '.com']     
+    dictionary = ['4','lain']
+    #dfile = open('dictionary.txt', 'r')
+    #dictionary = dfile.readlines()
+    for word in dictionary:
+        for adress in ending:
+            link = "http://{}chan{}".format(word, adress)  
+            try:
+                r = requests.get(link)
+                soup = BeautifulSoup(r.content, 'html5lib')
+                if 'domain' and 'for sale' in soup.prettify():
+                    continue
+                else:
+                    history = r.history
+                    for item in history:
+                        code = item.status_code
+                        if code != 302:
+                            links.append(link)
+                    else: 
+                        continue
+            except requests.exceptions.ConnectionError:
+                continue
+    return links
+
+#print(spiderchan())
